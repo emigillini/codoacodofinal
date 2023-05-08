1bf209b82c6a553785572f2e223b76c5a0d8902c
Emiliano R Gillini <emigillini@hotmail.com>
2023-05-08 19:40:09 -0300

    prodsOk

--- js.js
+++ js.js
@@ -7,8 +7,11 @@ x.addEventListener("click", () => {
   console.log(prod);
 });
 */
+
+import { productos } from "./productos.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-  let numRandom = parseInt(Math.random() * 100000);
+  let numRandom = Math.round( (Math.random() * 100000));
   let codigo = "XX " + numRandom + " ZZ";
   document.getElementById("codigo").innerText = codigo;
 });
@@ -18,3 +21,35 @@ document.addEventListener("click", () => {
   let bot = document.getElementById("banner");
   bot.style.display = "none";
 });
+
+
+
+const contenedorProductos = document.querySelector("#productos");
+
+function mostrarProductos() {
+  productos.forEach((producto) => {
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("producto");
+
+    const imagen = document.createElement("img");
+    imagen.src = producto.imagen;
+    imagen.alt = producto.nombre;
+    tarjeta.appendChild(imagen);
+
+    const nombre = document.createElement("h3");
+    nombre.innerText = producto.nombre;
+    tarjeta.appendChild(nombre);
+
+    const descripcion = document.createElement("p");
+    descripcion.innerText = producto.descripcion;
+    tarjeta.appendChild(descripcion);
+
+    const precio = document.createElement("p");
+    precio.innerText = `$${producto.precio}`;
+    tarjeta.appendChild(precio);
+
+    contenedorProductos.appendChild(tarjeta);
+  });
+}
+
+mostrarProductos();
