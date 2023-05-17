0ddde326d7ef1c9767879c35e28260c1f72c3fc6
Emiliano R Gillini <emigillini@hotmail.com>
2023-05-17 19:55:13 -0300

    entrega

--- contacto.js
+++ contacto.js
@@ -1,72 +1,39 @@
-const form = document.getElementById("myForm");
-const nameInput = document.getElementById("name");
-const emailInput = document.getElementById("email");
-const phoneInput = document.getElementById("phone");
-const messageInput = document.getElementById("message");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+var consultas = [];
 
-  const data = {}; // Creamos un objeto vacío para guardar los valores del formulario
+var form = document.getElementById("myForm");
 
-  if (nameInput.value === "") {
-    showError(nameInput, "El nombre es requerido");
-  } else {
-    showSuccess(nameInput);
-    data.name = nameInput.value; // Asignamos el valor del campo "name" al objeto "data"
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || phone === "") {
+    alert("Por favor, completa los campos obligatorios");
+    return; 
   }
 
-  if (emailInput.value === "") {
-    showError(emailInput, "El correo electrónico es requerido");
-  } else if (!isValidEmail(emailInput.value)) {
-    showError(emailInput, "Ingrese un correo electrónico válido");
-  } else {
-    showSuccess(emailInput);
-    data.email = emailInput.value; // Asignamos el valor del campo "email" al objeto "data"
+  var emailRegex = /^\S+@\S+\.\S+$/;
+  if (!emailRegex.test(email)) {
+    alert("Por favor, ingresa un correo electrónico válido");
+    return; 
   }
 
-  if (phoneInput.value === "") {
-    showError(phoneInput, "El teléfono es requerido");
-  } else if (!isValidPhone(phoneInput.value)) {
-    showError(phoneInput, "Ingrese un teléfono válido");
-  } else {
-    showSuccess(phoneInput);
-    data.phone = phoneInput.value; // Asignamos el valor del campo "phone" al objeto "data"
-  }
+  var consulta = {
+    name: name,
+    email: email,
+    phone: phone,
+    message: message
+  };
 
-  if (messageInput.value === "") {
-    showError(messageInput, "El mensaje es requerido");
-  } else {
-    showSuccess(messageInput);
-    data.message = messageInput.value; // Asignamos el valor del campo "message" al objeto "data"
-  }
+  consultas.push(consulta);
 
-  console.log(data); // Imprimimos el objeto "data" en la consola
+  form.reset();
+
   alert(
-    `Nombre: ${data.name}\nCorreo electrónico: ${data.email}\nTeléfono: ${data.phone}\nMensaje: ${data.message}`
+    `Nombre: ${consulta.name}\nCorreo electrónico: ${consulta.email}\nTeléfono: ${consulta.phone}\nMensaje: ${consulta.message}`
   );
 });
-
-function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control error";
-  const errorMessage = formControl.querySelector("small");
-  errorMessage.innerText = message;
-}
-
-function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control success";
-}
-
-function isValidEmail(email) {
-  if (email.includes("@")) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function isValidPhone(phone) {
-  return /^\d+$/.test(phone);
-}
